23dd21dce89ad3d9ba95fb2631093444082f70eb
Kevin Jackson <jackson.kevinpaul@gmail.com>
2025-03-17 12:03:30 -0300

    Primera modificacion del ejercicio

--- ejercicio4.py
+++ ejercicio4.py
@@ -25,6 +25,7 @@ answers = [
 # Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
+
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
 
@@ -32,24 +33,31 @@ for _ in range(3):
     question_index = random.randint(0, len(questions) - 1)
 
 # Se muestra la pregunta y las respuestas posibles
-print(questions[question_index])
-for i, answer in enumerate(answers[question_index]):
-    print(f"{i + 1}. {answer}")
+    print(questions[question_index])
+    for i, answer in enumerate(answers[question_index]):
+        print(f"{i + 1}. {answer}")
 
 # El usuario tiene 2 intentos para responder correctamente
-for intento in range(2):
-    user_answer = int(input("Respuesta: ")) - 1
-
-# Se verifica si la respuesta es correcta
-    if user_answer == correct_answers_index[question_index]:
-        print("¡Correcto!")
-        break
-else:
-
+    for intento in range(2):
+        try:    
+            user_answer = int(input("Respuesta: ")) - 1
+# Si el valor que se ingresa es invalido, corto el programa.
+        except ValueError:
+            print("Respuesta invalida.")
+            exit(1)
+# Se verifica si los valores son validos
+        if user_answer < 0 or user_answer > 3:
+            print("Respuesta invalida.")
+            exit(1)       
+# Se verifica si la respuesta es correcta            
+        if user_answer == correct_answers_index[question_index]:
+            print("¡Correcto!")
+            break
+    else:
 # Si el usuario no responde correctamente después de 2 intentos,
 # se muestra la respuesta correcta
-    print("Incorrecto. La respuesta correcta es:")
-    print(answers[question_index][correct_answers_index[question_index]])
+        print("Incorrecto. La respuesta correcta es:")
+        print(answers[question_index][correct_answers_index[question_index]])
 
 # Se imprime un blanco al final de la pregunta
 print()
